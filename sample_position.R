5b542674305d800d6964b9fb36126e756da1d914
Michał Cisek <michal.cisek@pwc.com>
2019-08-06 13:49:05 +0200

    reorganised project structure

--- sample_position.R
+++
@@ -1,9 +0,0 @@
-sample_position <- function(){
-  positions <- c("GKP", "DEF", "MID", "FWD")
-  #probabilities based on the number of available players selected in FPL
-  probs <- c(2/15, 1/3, 1/3, 1/5)
-  
-  position <- sample(positions, 1, prob = probs)  
-  
-  return(position)
-}
