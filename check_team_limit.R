094800b988ca5ca2627fd31b32a8469a6664424b
Michał Cisek <michal.cisek@pwc.com>
2019-08-06 13:49:55 +0200

    reorganised project structure

--- check_team_limit.R
+++
@@ -1,7 +0,0 @@
-check_team_limit <- function(player, team, data){
-  used <- data[data$code %in% team, "team"]
-  team_sum <- sum(used == data[data$code == player, "team"])
-  
-  if(team_sum >= 3) return("exceeded")
-  else return("not exceeded")
-}
